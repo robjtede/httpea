cb3f56da57740d1f553b80a1a617a3bed0db6cd1
Rob Ede <robjtede@icloud.com>
2023-03-16 04:45:40 +0000

    initial commit

---
+++ crates/httpea/examples/h3-server.rs
@@ -0,0 +1,212 @@
+use std::{io, net::SocketAddr, path::PathBuf, sync::Arc};
+
+use bytes::{Bytes, BytesMut};
+use futures_util::StreamExt;
+use http::{Request, StatusCode};
+use rustls::{Certificate, PrivateKey};
+use structopt::StructOpt;
+use tokio::{fs::File, io::AsyncReadExt};
+use tracing::{error, info, trace_span};
+
+use h3::{error::ErrorLevel, quic::BidiStream, server::RequestStream};
+use h3_quinn::quinn;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "server")]
+struct Opt {
+    #[structopt(
+        name = "dir",
+        short,
+        long,
+        help = "Root directory of the files to serve. \
+                If omitted, server will respond OK."
+    )]
+    pub root: Option<PathBuf>,
+
+    #[structopt(
+        short,
+        long,
+        default_value = "[::1]:4433",
+        help = "What address:port to listen for new connections"
+    )]
+    pub listen: SocketAddr,
+
+    #[structopt(flatten)]
+    pub certs: Certs,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Certs {
+    #[structopt(
+        long,
+        short,
+        default_value = "httpea/examples/cert.pem",
+        help = "Certificate for TLS. If present, `--key` is mandatory."
+    )]
+    pub cert: PathBuf,
+
+    #[structopt(
+        long,
+        short,
+        default_value = "httpea/examples/key.pem",
+        help = "Private key for the certificate."
+    )]
+    pub key: PathBuf,
+}
+
+static ALPN: &[u8] = b"h3";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::EXIT)
+        .with_writer(std::io::stderr)
+        .init();
+
+    // process cli arguments
+
+    let opt = Opt::from_args();
+
+    let root = if let Some(root) = opt.root {
+        if !root.is_dir() {
+            return Err(format!("{}: is not a readable directory", root.display()).into());
+        } else {
+            info!("serving {}", root.display());
+            Arc::new(Some(root))
+        }
+    } else {
+        Arc::new(None)
+    };
+
+    let Certs { cert, key } = opt.certs;
+
+    // create quinn server endpoint and bind UDP socket
+
+    // both cert and key must be DER-encoded
+    let mut buf = io::BufReader::new(std::fs::File::open(cert)?);
+    let cert = Certificate(rustls_pemfile::certs(&mut buf)?.pop().unwrap());
+    let mut buf = io::BufReader::new(std::fs::File::open(key)?);
+    let key = PrivateKey(rustls_pemfile::pkcs8_private_keys(&mut buf)?.pop().unwrap());
+
+    let mut tls_config = rustls::ServerConfig::builder()
+        .with_safe_default_cipher_suites()
+        .with_safe_default_kx_groups()
+        .with_protocol_versions(&[&rustls::version::TLS13])
+        .unwrap()
+        .with_no_client_auth()
+        .with_single_cert(vec![cert], key)?;
+
+    tls_config.max_early_data_size = u32::MAX;
+    tls_config.alpn_protocols = vec![ALPN.into()];
+
+    let server_config = quinn::ServerConfig::with_crypto(Arc::new(tls_config));
+    let (endpoint, mut incoming) = quinn::Endpoint::server(server_config, opt.listen)?;
+
+    info!("listening on {}", opt.listen);
+
+    // handle incoming connections and requests
+
+    while let Some(new_conn) = incoming.next().await {
+        trace_span!("New connection being attempted");
+
+        let root = root.clone();
+
+        tokio::spawn(async move {
+            match new_conn.await {
+                Ok(conn) => {
+                    info!("new connection established");
+
+                    let mut h3_conn = h3::server::Connection::new(h3_quinn::Connection::new(conn))
+                        .await
+                        .unwrap();
+
+                    loop {
+                        match h3_conn.accept().await {
+                            Ok(Some((req, stream))) => {
+                                info!("new request: {:#?}", req);
+
+                                let root = root.clone();
+
+                                tokio::spawn(async {
+                                    if let Err(e) = handle_request(req, stream, root).await {
+                                        error!("handling request failed: {}", e);
+                                    }
+                                });
+                            }
+
+                            // indicating no more streams to be received
+                            Ok(None) => {
+                                break;
+                            }
+
+                            Err(err) => {
+                                error!("error on accept {}", err);
+                                match err.get_error_level() {
+                                    ErrorLevel::ConnectionError => break,
+                                    ErrorLevel::StreamError => continue,
+                                }
+                            }
+                        }
+                    }
+                }
+                Err(err) => {
+                    error!("accepting connection failed: {:?}", err);
+                }
+            }
+        });
+    }
+
+    // shut down gracefully
+    // wait for connections to be closed before exiting
+    endpoint.wait_idle().await;
+
+    Ok(())
+}
+
+async fn handle_request<T>(
+    req: Request<()>,
+    mut stream: RequestStream<T, Bytes>,
+    serve_root: Arc<Option<PathBuf>>,
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    T: BidiStream<Bytes>,
+{
+    let (status, to_serve) = match serve_root.as_deref() {
+        None => (StatusCode::OK, None),
+        Some(_) if req.uri().path().contains("..") => (StatusCode::NOT_FOUND, None),
+        Some(root) => {
+            let to_serve = root.join(req.uri().path().strip_prefix('/').unwrap_or(""));
+            match File::open(&to_serve).await {
+                Ok(file) => (StatusCode::OK, Some(file)),
+                Err(e) => {
+                    error!("failed to open: \"{}\": {}", to_serve.to_string_lossy(), e);
+                    (StatusCode::NOT_FOUND, None)
+                }
+            }
+        }
+    };
+
+    let resp = http::Response::builder().status(status).body(()).unwrap();
+
+    match stream.send_response(resp).await {
+        Ok(_) => {
+            info!("successfully respond to connection");
+        }
+        Err(err) => {
+            error!("unable to send response to connection peer: {:?}", err);
+        }
+    }
+
+    if let Some(mut file) = to_serve {
+        loop {
+            let mut buf = BytesMut::with_capacity(4096 * 10);
+            if file.read_buf(&mut buf).await? == 0 {
+                break;
+            }
+            stream.send_data(buf.freeze()).await?;
+        }
+    }
+
+    Ok(stream.finish().await?)
+}
