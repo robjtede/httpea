a60e5e572651082b07de7405807b9f202ef98e6d
Rob Ede <robjtede@icloud.com>
2026-04-21 13:13:04 +0100

    feat(http-request-target): improve error enum

--- crates/http-request-target/src/error.rs
+++ crates/http-request-target/src/error.rs
@@ -1,5 +1,30 @@
-/// Error that occurs when parsing request target fails.
-#[derive(Debug, Clone)]
+//! Error types for request-target parsing.
+
+/// Error that occurs when parsing a request target fails.
+///
+/// This error type is returned by [`RequestTarget::try_from_slice`] when the input does not
+/// conform to any of the four request-target forms defined in [RFC 9112].
+///
+/// [`RequestTarget::try_from_slice`]: crate::RequestTarget::try_from_slice
+/// [RFC 9112]: https://datatracker.ietf.org/doc/html/rfc9112
+#[derive(Debug, Clone, PartialEq, Eq)]
 #[non_exhaustive]
-#[expect(dead_code)]
-pub struct ParseRequestTargetError;
+pub enum ParseRequestTargetError {
+    /// Input was empty.
+    EmptyInput,
+
+    /// Input did not match any recognized request-target form.
+    InvalidTarget,
+
+    /// Input was incomplete (only when parsing with `Partial`).
+    Incomplete,
+}
+
+impl_more::impl_display_enum! {
+    ParseRequestTargetError:
+    EmptyInput => "Input was empty",
+    InvalidTarget => "Input did not match any recognized request-target form",
+    Incomplete => "Input was incomplete",
+}
+
+impl_more::impl_leaf_error!(ParseRequestTargetError);
