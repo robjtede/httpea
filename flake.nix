6c9ce241e4d2553d5a31eec760f05c1baa15c484
Rob Ede <robjtede@icloud.com>
2026-07-31 15:06:18 +0100

    chore: use shared Rust Just MSRV recipes

--- flake.nix
+++ flake.nix
@@ -2,10 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    x52 = {
+      url = "github:x52dev/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.x52.flakeModules.default ];
+
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       perSystem = { pkgs, config, lib, ... }: {
@@ -16,12 +23,15 @@
             config.formatter
             pkgs.cargo-nextest
             pkgs.cargo-shear
+            pkgs.jq
             pkgs.just
             pkgs.prettier
             pkgs.taplo
           ] ++ lib.optionals pkgs.stdenv.isDarwin [
             pkgs.pkgsBuildHost.libiconv
           ];
+
+          shellHook = config.x52.justRust.shellHook;
         };
       };
     };
